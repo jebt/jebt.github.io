2b7903f97c3b016448f988aeb919957eccc7b9e2
jebt <roelantvanderhilst@gmail.com>
2020-12-30 00:31:51 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,45 +1,18 @@
-
 function setup() {
   createCanvas(innerWidth, innerHeight);
   background(255)
   frameRate(10)
-
-
-  for (let i = 0; i < 1500; i++) {
-   
-   
-    if(i%20 == 0) {
-      fill(random(255),random(255),random(255))
-    } else {
-      fill(255)
-    }
-  // noStroke()
-    // rect(random(1,500), random(1,500),random(15,150), random(15,150))    
-    stroke(0, random(0,255));
-    rect(random(1,width), random(1,height),random(5,250), random(5,250))
-  }
-
-
-
   tint(255);
-
-  // saveCanvas('myCanvas', 'jpg');
 }
 
 function draw() {
    for (let i = 0; i < 1500; i++) {
-   
-   
     if(i%10 == 0) {
       fill(random(255),random(255),random(255))
     } else {
       fill(255)
     }
-  // noStroke()
-    // rect(random(1,500), random(1,500),random(15,150), random(15,150))    
     stroke(0, random(0,255));
     rect(random(1,width), random(1,height),random(5,250), random(5,250))
   }
-  
-  
 }
