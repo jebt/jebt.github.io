bf790c83f570064158384c0c99d887850c3938cd
jebt <roelantvanderhilst@gmail.com>
2020-12-30 01:15:56 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 function setup() {
   createCanvas(innerWidth, innerHeight);
-  background(255)
   frameRate(3)
   noStroke();
 }
 
 function draw() {
+  background(255)
    for (let i = 0; i < 1500; i++) {
     if(i%10 == 0) {
       fill(random(255),random(255),random(255))
